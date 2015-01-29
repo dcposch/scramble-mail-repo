f20c5e9ef0a8660cd9e92b2eea5fd25fef3ee1bc
DC <dcposch@dcpos.ch>
2015-01-29 01:18:56 -0800

    Add semistandard

--- example.js
+++ example.js
@@ -6,10 +6,10 @@ var ScrambleMailRepo = require('./index.js');
 
 // Create a temporary folder, where we'll build the index
 var fromDir, toDir;
-if(process.argv.length == 2) {
-  fromDir = "test/";
+if (process.argv.length === 2) {
+  fromDir = 'test/';
   toDir = tmp.in(os.tmpdir());
-} else if (process.argv.length == 4) {
+} else if (process.argv.length === 4) {
   fromDir = process.argv[2];
   toDir = process.argv[3];
 } else {
@@ -19,8 +19,8 @@ demoSaveEmails(fromDir, toDir);
 
 // Save a folder full of raw emails into an an
 // empty folder, where we'll build an email index
-function demoSaveEmails(fromDir, toDir){
-  console.log("Reading mail from %s, indexing in %s", fromDir, toDir);
+function demoSaveEmails (fromDir, toDir) {
+  console.log('Reading mail from %s, indexing in %s', fromDir, toDir);
 
   // Make a new repo to populate the empty temp folder
   var repo = new ScrambleMailRepo(toDir);
@@ -28,32 +28,33 @@ function demoSaveEmails(fromDir, toDir){
   // Grab a few example emails and stick them into the repo
   // In each case, the email is the full RFC2822 message
   var files = fs.readdirSync(fromDir);
-  console.time("Indexing %d messages", files.length);
-  console.time("Indexing messages");
+  console.time('Indexing %d messages', files.length);
+  console.time('Indexing messages');
 
   // Index emails in parallel
-  var numDone = 0, nextIx = 0;
-  var saveEmail = function(){
-    if(nextIx >= files.length){
+  var numDone = 0;
+  var nextIx = 0;
+  var saveEmail = function () {
+    if (nextIx >= files.length) {
       return;
     }
     var file = files[nextIx++];
-    var rawEmailStream = fs.createReadStream(fromDir+"/"+file);
+    var rawEmailStream = fs.createReadStream(fromDir + '/' + file);
     repo.saveRawEmail(rawEmailStream, onEmailSaved);
-  }
+  };
   // Only process 40 at a time
-  for(var i = 0; i < 40; i++){
+  for (var i = 0; i < 40; i++) {
     saveEmail();
   }
 
-  function onEmailSaved(err) {
-    if(err) {
-      console.error("Error saving email", err);
+  function onEmailSaved (err) {
+    if (err) {
+      console.error('Error saving email', err);
     }
     process.stdout.write('.');
-    if(++numDone == files.length) {
+    if (++numDone === files.length) {
       process.stdout.write('\n\n');
-      console.timeEnd("Indexing messages");
+      console.timeEnd('Indexing messages');
       // Finally, we're done building the email index
       // Next, show how to query it
       demoQueryEmails(repo);
@@ -63,27 +64,27 @@ function demoSaveEmails(fromDir, toDir){
   }
 }
 
-function demoQueryEmails(repo){
+function demoQueryEmails (repo) {
   // Full text search
-  repo.search("fifth of november", printResults);
+  repo.search('fifth of november', printResults);
   // Full text search includes From and To
-  repo.search("root@eruditorum.org", printResults);
-  // You can also search explicitly. "to:" queries include the CC header.
-  repo.search("to:root@eruditorom.org", printResults);
-  repo.search("to:root", printResults);
+  repo.search('root@eruditorum.org', printResults);
+  // You can also search explicitly. 'to:' queries include the CC header.
+  repo.search('to:root@eruditorom.org', printResults);
+  repo.search('to:root', printResults);
   // Search for an exact string
-  repo.search("\"remember, remember\"", printResults);
+  repo.search('\"remember, remember\"', printResults);
 
   // Get the inbox
-  repo.search("label:\\scramble\\inbox", printResults);
+  repo.search('label:\\scramble\\inbox', printResults);
   // Get all sent mail
-  repo.search("label:\\scramble\\sent", printResults);
+  repo.search('label:\\scramble\\sent', printResults);
 
   // Fetch a given thread
-  repo.getThread("herpThread", printResults);
+  repo.getThread('herpThread', printResults);
 
   // Fetch a given message
-  repo.getMessage("herp", printResults);
+  repo.getMessage('herp', printResults);
 
   demoUserSearch(repo);
 }
@@ -93,30 +94,29 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-function demoUserSearch(repo){
-  rl.question("> ", function(query){
-    repo.search(query, function(err, results){
+function demoUserSearch (repo) {
+  rl.question('> ', function (query) {
+    repo.search(query, function (err, results) {
       printResults(err, results);
       demoUserSearch(repo);
     });
   });
 }
 
-function printResults(err, results){
-  if(err) {
-    console.error("Error searching emails", err);
+function printResults (err, results) {
+  if (err) {
+    console.error('Error searching emails', err);
     return;
   }
-  if(results && results.hasOwnProperty("length")){
-    console.log("Got "+results.length+" result(s)");
-    results.forEach(function(result){
+  if (results && results.hasOwnProperty('length')) {
+    console.log('Got ' + results.length + ' result(s)');
+    results.forEach(function (result) {
       console.log(result);
     });
-  } else if (results){
-    console.log("Got the result");
+  } else if (results) {
+    console.log('Got the result');
     console.log(results);
   } else {
-    console.log("No result");
+    console.log('No result');
   }
 }
-
--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-var fs = require("fs");
-var stream = require("stream");
-var crypto = require("crypto");
-var sqlite3 = require("sqlite3").verbose();
-var MailParser = require("mailparser").MailParser;
-require("look").start();
+var fs = require('fs');
+var stream = require('stream');
+var crypto = require('crypto');
+var sqlite3 = require('sqlite3').verbose();
+var MailParser = require('mailparser').MailParser;
 
-module.exports = function(dir) {
-  var dbFilename = dir+"/index.sqlite";
+module.exports = function (dir) {
+  var dbFilename = dir + '/index.sqlite';
   var db = new sqlite3.Database(dbFilename);
   initDatabase();
 
-  function initDatabase() {
+  function initDatabase () {
     // Create any new tables, or create all tables if DB was newly created
-    var schemaSql = fs.readFileSync("schema.sql", {"encoding":"utf8"});
+    var schemaSql = fs.readFileSync('schema.sql', {encoding: 'utf8'});
     // Don't run any other queries until the DB is initialized
-    db.serialize(function(){
-      db.exec(schemaSql, function(err){
-        if(err == null){
-          console.log("Created the email index sqlite DB successfully");
+    db.serialize(function () {
+      db.exec(schemaSql, function (err) {
+        if (err === null) {
+          console.log('Created the email index sqlite DB successfully');
         } else {
-          console.error("Error creating sqlite DB "+dbFilename, err);
+          console.error('Error creating sqlite DB ' + dbFilename, err);
         }
       });
     });
@@ -29,116 +28,119 @@ module.exports = function(dir) {
    * Returns a non-cryptographically-strong random string.
    * Length n base64 chars, so 6n bits.
    */
-  function pseudoRandomBase64(numChars){
+  function pseudoRandomBase64 (numChars) {
     var buf = crypto.pseudoRandomBytes(numChars);
-    return buf.toString("base64").substring(0, numChars);
+    return buf.toString('base64').substring(0, numChars);
   }
 
-  function saveMailObj(mailObj, cb) {
-      var errs = [], numDone = 0, numQueries = 3;
-      var subCallback = function(err) {
-        if(err) {
-          errs.push(err);
-        }
-        if(++numDone == numQueries) {
-          cb(errs[0] || null);
-        }
-      };
+  function saveMailObj (mailObj, cb) {
+    var errs = [];
+    var numDone = 0;
+    var numQueries = 3;
+    var subCallback = function (err) {
+      if (err) {
+        errs.push(err);
+      }
+      if (++numDone === numQueries) {
+        cb(errs[0] || null);
+      }
+    };
 
-      // Insert Message
-      var messageId = mailObj.headers["message-id"] || null;
-      var scrambleMailId = messageId || pseudoRandomBase64(40);
-      var scrambleThreadId = "dummy-thread-id"; //TODO: threading
-      var timestamp = new Date().toISOString();
-      var fromAddress = mailObj.headers["from"];
-      var toAddress = mailObj.headers["to"];
-      var ccAddress = mailObj.headers["cc"];
-      var bccAddress = mailObj.headers["bcc"];
-      var subject = mailObj.subject;
-      var snippet = mailObj.text;
-      db.run("insert or ignore into Message (scrambleMailId, scrambleThreadId, timestamp, messageId, "+
-          "fromAddress, toAddress, ccAddress, bccAddress, subject, snippet) "+
-          "values (?,?,?,?,?,?,?,?,?,?)",
-          scrambleMailId, scrambleThreadId, timestamp, messageId,
-          fromAddress, toAddress, ccAddress, bccAddress, subject, snippet, subCallback);
+    // Insert Message
+    var messageId = mailObj.headers['message-id'] || null;
+    var scrambleMailId = messageId || pseudoRandomBase64(40);
+    var scrambleThreadId = 'dummy-thread-id'; // TODO: threading
+    var timestamp = new Date().toISOString();
+    var fromAddress = mailObj.headers['from'];
+    var toAddress = mailObj.headers['to'];
+    var ccAddress = mailObj.headers['cc'];
+    var bccAddress = mailObj.headers['bcc'];
+    var subject = mailObj.subject;
+    var snippet = mailObj.text;
+    db.run('insert or ignore into Message (scrambleMailId, scrambleThreadId, timestamp, messageId, ' +
+      'fromAddress, toAddress, ccAddress, bccAddress, subject, snippet) ' +
+      'values (?,?,?,?,?,?,?,?,?,?)',
+      scrambleMailId, scrambleThreadId, timestamp, messageId,
+      fromAddress, toAddress, ccAddress, bccAddress, subject, snippet, subCallback);
 
+    // Index for full-text search
+    var searchBody = subject + '\n\n' + mailObj.text;
+    db.run('insert into MessageSearch (scrambleMailId, subject, fromAddress, toAddress, searchBody) ' +
+      'values (?,?,?,?,?)',
+      scrambleMailId, subject, fromAddress, toAddress, searchBody, subCallback);
 
-      // Index for full-text search
-      var searchBody = subject + "\n\n" + mailObj.text;
-      db.run("insert into MessageSearch (scrambleMailId, subject, fromAddress, toAddress, searchBody) "+
-          "values (?,?,?,?,?)",
-          scrambleMailId, subject, fromAddress, toAddress, searchBody, subCallback);
+    // Update Contact
+    var contacts = [mailObj.from, mailObj.to, mailObj.cc, mailObj.bcc].reduce(function (a, b) {
+        return b ? a.concat(b) : a;
+      }, []);
+    saveContacts(contacts, subCallback);
 
-      // Update Contact
-      var contacts = [mailObj.from, mailObj.to, mailObj.cc, mailObj.bcc].reduce(function(a,b){
-            if(b) return a.concat(b);
-            return a;
-          }, []);
-      saveContacts(contacts, subCallback);
-
-      //TODO: PGP decryption
-      //TODO: Update MessageLabel
+    // TODO: PGP decryption
+    // TODO: Update MessageLabel
   }
 
   /**
-   * Takes a list of named email addresses, each {address:..., name:...} 
+   * Takes a list of named email addresses, each {address:..., name:...}
    * Makes sure each address exists in the Contact table.
    * Counts how often each name is used with a given address.
    */
-  function saveContacts(contacts, cb) {
-      console.log("Saving contacts", contacts);
-      var errs = [], numDone = 0, numQueries = 3*contacts.length;
-      var subCallback = function(err) {
-        if(err) {
-          errs.push(err);
-        }
-        if(++numDone == numQueries) {
-          cb(errs[0] || null);
-        }
-      };
+  function saveContacts (contacts, cb) {
+    console.log('Saving contacts', contacts);
+    var errs = [];
+    var numDone = 0;
+    var numQueries = 3 * contacts.length;
+    var subCallback = function (err) {
+      if (err) {
+        errs.push(err);
+      }
+      if (++numDone === numQueries) {
+        cb(errs[0] || null);
+      }
+    };
 
-      var stmtC = db.prepare("insert or ignore into Contact (emailAddress) values (?)");
-      var stmtCN = db.prepare("insert or ignore into ContactName (emailAddress, name) values (?,?)");
-      var stmtCNInc = db.prepare("update ContactName set numMessages=numMessages+1 where emailAddress=? and name=?");
-      contacts.forEach(function(contact){
-        var addr = contact.address.toLowerCase();
-        stmtC.run(addr, subCallback);
-        stmtCN.run(addr, contact.name, subCallback);
-        stmtCNInc.run(addr, contact.name, subCallback);
-      });
-      stmtC.finalize();
-      stmtCN.finalize();
-      stmtCNInc.finalize();
+    var stmtC = db.prepare('insert or ignore into Contact (emailAddress) values (?)');
+    var stmtCN = db.prepare('insert or ignore into ContactName (emailAddress, name, numMessages) values (?,?,0)');
+    var stmtCNInc = db.prepare('update ContactName set numMessages=numMessages+1 where emailAddress=? and name=?');
+    contacts.forEach(function (contact) {
+      var addr = contact.address.toLowerCase();
+      stmtC.run(addr, subCallback);
+      stmtCN.run(addr, contact.name, subCallback);
+      stmtCNInc.run(addr, contact.name, subCallback);
+    });
+    stmtC.finalize();
+    stmtCN.finalize();
+    stmtCNInc.finalize();
   }
 
   /**
    * Saves a raw RFC2822 email.
    *
    * The first argument can be either a string or a stream.
-   * The stream should be ASCII encoded. (Remember that ASCII is a strict 
+   * The stream should be ASCII encoded. (Remember that ASCII is a strict
    * subset of UTF-8, so UTF-8 works too. All code points sholuld be <= 127.)
    *
    * The second argument is a callback(err, mailObj). It is not called until
    * the email has been successfully written to disk.
    **/
-  this.saveRawEmail = function(email, cb) {
+  this.saveRawEmail = function (email, cb) {
     var mailparser = new MailParser();
-    mailparser.on("headers", function(err){
+    mailparser.on('headers', function (err) {
+      console.error(err);
     });
-    mailparser.on("error", function(err){
+    mailparser.on('error', function (err) {
       cb(err, null);
     });
-    mailparser.on("end", function(mailObj){
+    mailparser.on('end', function (mailObj) {
       saveMailObj(mailObj, cb);
     });
 
-    if(email instanceof stream.Readable) {
+    if (email instanceof stream.Readable) {
       email.pipe(mailparser);
     } else {
       mailparser.write(email);
       mailparser.end();
     }
-  }
+  };
 
   /**
    * Email full-text search. See README for query syntax.
@@ -146,45 +148,45 @@ module.exports = function(dir) {
    * The limit and offset args are optional.
    * The callback takes (err, array of email objs).
    */
-  this.search = function(query, limit, offset, cb){
-    if(arguments.length == 2){
+  this.search = function (query, limit, offset, cb) {
+    if (arguments.length === 2) {
       cb = limit;
       limit = 10;
       offset = 0;
     }
-    if(query === ""){
+    if (query === '') {
       cb(null, null);
     } else {
-      db.all("select scrambleMailId, fromAddress, toAddress, subject, snippet(MessageSearch) as snippet "+
-          "from MessageSearch where searchBody match ? limit ? offset ?", 
-          query, limit, offset, function(err, results){
-        if(err) {
+      db.all('select scrambleMailId, fromAddress, toAddress, subject, snippet(MessageSearch) as snippet ' +
+          'from MessageSearch where searchBody match ? limit ? offset ?',
+          query, limit, offset, function (err, results) {
+        if (err) {
           return cb(err, null);
         }
-        var mailIds = results.map(function(x) { return x.scrambleMailId; });
-        db.all("select * from Message where scrambleMailId in ('" + mailIds.join("','") + "')", cb);
-      }); 
+        var mailIds = results.map(function (x) { return x.scrambleMailId; });
+        db.all('select * from Message where scrambleMailId in (\'' + mailIds.join('', '') + '\')', cb);
+      });
     }
-  }
+  };
 
-  this.getThread = function(threadId, cb){
-    db.all("select * from Message where scrambleThreadId=?", threadId, cb);
-  }
+  this.getThread = function (threadId, cb) {
+    db.all('select * from Message where scrambleThreadId=?', threadId, cb);
+  };
 
   /**
    * Fetches the unique message with the given ID.
    * Calls cb(err, message)
    * Exactly one of err or message will be null.
    */
-  this.getMessage = function(mailId, cb){
-    db.get("select * from Message where scrambleMailId=?", mailId, function(err, row){
-      if(err){
+  this.getMessage = function (mailId, cb) {
+    db.get('select * from Message where scrambleMailId=?', mailId, function (err, row) {
+      if (err) {
         cb(err, null);
       }
-      if(!row){
-        cb("Couldn't find mail ID "+mailId, null);
+      if (!row) {
+        cb('Couldn\'t find mail ID ' + mailId, null);
       }
       cb(null, row);
     });
-  }
+  };
 };
